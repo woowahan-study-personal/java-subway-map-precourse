a3163213fcdec0d6b96e995cad304feeb2041c8f
dreamfinder2019 <dreamfinder2019@gmail.com>
2021-05-20 14:39:10 +0900

    feat(domain): Add nameValidation

--- src/main/java/subway/domain/Station.java
+++ src/main/java/subway/domain/Station.java
@@ -1,9 +1,11 @@
 package subway.domain;
 
 public class Station {
+
     private String name;
 
     public Station(String name) {
+        nameValidation(name);
         this.name = name;
     }
 
@@ -12,4 +14,10 @@ public class Station {
     }
 
     // 추가 기능 구현
+
+    private void nameValidation(String name) {
+        if (name.length() < 2 || name.length() > 5) {
+            throw new IllegalArgumentException("[ERROR]: 이름의 범위가 2 ~ 5 이내인 경우만 허용합니다.");
+        }
+    }
 }
