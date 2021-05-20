8c768624d9142be451162438eeef85bf17e3be4e
dreamfinder2019 <dreamfinder2019@gmail.com>
2021-05-20 13:59:49 +0900

    feat(domain): Line - Add stations

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -1,7 +1,11 @@
 package subway.domain;
 
+import java.util.LinkedList;
+
 public class Line {
+
     private String name;
+    private LinkedList<String> stations = new LinkedList<>();
 
     public Line(String name) {
         this.name = name;
