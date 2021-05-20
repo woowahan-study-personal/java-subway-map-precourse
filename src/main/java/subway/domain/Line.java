6729b60f149e899131080934d94fc45ab441fb87
dreamfinder2019 <dreamfinder2019@gmail.com>
2021-05-20 14:24:25 +0900

    feat(domain): Line - Add contructors and validators

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -5,15 +5,27 @@ import java.util.LinkedList;
 public class Line {
 
     private String name;
-    private LinkedList<String> stations = new LinkedList<>();
+    private LinkedList<Station> stations = new LinkedList<>();
 
     public Line(String name) {
         this.name = name;
+    }
+
+    public Line(String name, Station from, Station to) {
+        nameValidation(name);
+        this.name = name;
+        this.stations.add(from);
+        this.stations.add(to);
     }
 
     public String getName() {
         return name;
     }
 
-    // 추가 기능 구현
+    private void nameValidation(String name) {
+        if (name.length() < 2 || name.length() > 5) {
+            throw new IllegalArgumentException("[ERROR]: 이름의 범위가 2 ~ 5 이내인 경우만 허용합니다.");
+        }
+    }
+
 }
