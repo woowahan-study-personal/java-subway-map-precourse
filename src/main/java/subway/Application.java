8fdcae666e1b45cb5a76683216c077ef17ae8b00
dreamfinder2019 <dreamfinder2019@gmail.com>
2021-05-20 11:39:54 +0900

    style(subway): Application - Add comment for TO-DOs

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -2,6 +2,18 @@ package subway;
 
 import java.util.Scanner;
 
+/*
+ TO-DO: 아래의 사전 등록 정보로 "반드시" 초기 설정을 하기
+
+ 1. 지하철역으로 교대역, 강남역, 역삼역, 남부터미널역, 양재역, 양재시민의숲역, 매봉역이 등록되어 있다.
+ 2. 지하철 노선으로 2호선, 3호선, 신분당선이 등록되어 있다.
+ 3. 노선에 역이 아래와 같이 등록되어 있다.(왼쪽 끝이 상행 종점)
+   - 2호선: 교대역 - 강남역 - 역삼역
+   - 3호선: 교대역 - 남부터미널역 - 양재역 - 매봉역
+   - 신분당선: 강남역 - 양재역 - 양재시민의숲역
+ */
+
+
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
