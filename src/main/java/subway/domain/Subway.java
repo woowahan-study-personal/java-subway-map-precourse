8b5d0a807afb9b23fec275aed7fef0a9e29e18a8
dreamfinder2019 <dreamfinder2019@gmail.com>
2021-05-22 23:44:46 +0900

    feat(domain): Subway

--- src/main/java/subway/domain/Subway.java
+++ src/main/java/subway/domain/Subway.java
@@ -9,11 +9,17 @@ public class Subway {
     }
 
     public void addStation(String stationName) {
-        stationRepository.addStation(stationName);
+        Station station = new Station(stationName);
+        stationRepository.addStation(station);
     }
 
     public void addLine(String name, String from, String to) {
         // TO-DO: 지하철 노선을 추가하는 로직을 구현한다
+        Station fromStation = stationRepository.getStation(from);
+        Station toStation = stationRepository.getStation(to);
+        Line line = new Line(name, fromStation, toStation);
+
+        lineRepository.addLine(line);
     }
 
     private boolean isOkayToAddStation(String lineName, String stationName) {
@@ -22,21 +28,10 @@ public class Subway {
     }
 
     public void addStationToLine(String lineName, String stationName, int pathIndex) {
-        // TO-DO: 구간을 등록하는 로직을 구현한다. 아래의 흐름도를 참조해가며 구현에 참조하도록 한다.
-
-        // 1) 노선을 탐색한다. 만약에 노선 이름에 해당되는 객체가 없으면 해당 노선이 없다는 메시지를 출력한다.
-        // 2) 추가할 역 이름을 입력한다. 이때, 역 이름에 해당되는 객체가 없다면 해당 역이 없다는 메시지를 출력하고 종료한다.
-        // 3) 그 다음 순서에 해당되는 정수를 입력하도록 한다.
-
-        // 노선 객체 1) 연동
-        // 역 객체 2) 연동
-
         if (isOkayToAddStation(lineName, stationName)) {
             lineRepository.addStationToLine(lineRepository.getModifiableLine(lineName),
                 stationRepository.getStation(stationName), pathIndex);
         }
-
-
     }
 }
 
