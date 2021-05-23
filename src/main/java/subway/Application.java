c3af5f7d748cf811cee11b5b555c888dcd0f8502
dreamfinder2019 <dreamfinder2019@gmail.com>
2021-05-23 12:46:00 +0900

    feat(subway): Application

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -1,6 +1,7 @@
 package subway;
 
 import java.util.Scanner;
+import subway.domain.Subway;
 
 /*
  TO-DO: 아래의 사전 등록 정보로 "반드시" 초기 설정을 하기
@@ -13,10 +14,39 @@ import java.util.Scanner;
    - 신분당선: 강남역 - 양재역 - 양재시민의숲역
  */
 
-
 public class Application {
+
+    private static Subway init() {
+        Subway subway = new Subway();
+
+        subway.addStation("교대역");
+        subway.addStation("강남역");
+        subway.addStation("역삼역");
+        subway.addStation("남부터미널역");
+        subway.addStation("양재역");
+        subway.addStation("양재시민의숲역");
+        subway.addStation("매봉역");
+        
+        subway.addLine("2호선", "교대역", "강남역");
+        subway.addLine("3호선", "교대역", "남부터미널역");
+        subway.addLine("신분당선", "강남역", "양재역");
+        
+        subway.addStationToLine("2호선", "역삼역", 2);
+        subway.addStationToLine("3호선", "양재역", 2);
+        subway.addStationToLine("3호선", "매봉역", 3);
+        subway.addStationToLine("신분당선", "양재시민의숲역", 2);
+
+        return subway;
+    }
+
+    private static void runApplication(Scanner sc) {
+        Subway subway = init();
+    }
+
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO: 프로그램 구현
+
+        runApplication(scanner);
     }
 }
