cea86e2f69458d4230513a7681a740d597ac006d
dreamfinder2019 <dreamfinder2019@gmail.com>
2021-05-23 12:56:36 +0900

    feat(subway): Application

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -45,8 +45,6 @@ public class Application {
 
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        // TODO: 프로그램 구현
-
         runApplication(scanner);
     }
 }
