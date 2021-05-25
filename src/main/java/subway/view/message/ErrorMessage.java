dd15c4c153a5ccdca9c8c8338c9976d60e9c384d
dreamfinder2019 <dreamfinder2019@gmail.com>
2021-05-25 16:21:36 +0900

    refactor: message - Error message updated

--- src/main/java/subway/view/message/ErrorMessage.java
+++ src/main/java/subway/view/message/ErrorMessage.java
@@ -9,10 +9,13 @@ public class ErrorMessage {
 
     private static final String failedRequest = "요청을 처리할 수 없습니다.";
     private static final String requiresIntegerOnly = "정수만 입력하실 수 있습니다";
+    private static final String contentLengthOutOfRange = "이름의 범위가 2 ~ 8 이내인 경우만 허용합니다.";
+    private static final String cannotFound = "요청하신 데이터를 찾을 수 없습니다.";
 
     private static final String stationAlreadyExists = "이미 등록된 역입니다.";
     private static final String lineAlreadyExists = "이미 등록된 노선입니다.";
     private static final String stationAlreadyExistsInLine = "해당 역은 이미 노선에 존재합니다.";
+
 
     public static String failedRequestErrorMessage() {
         return errorHeader + failedRequest;
@@ -28,6 +31,14 @@ public class ErrorMessage {
 
     public static String stationAlreadyExistsInLineErrorMessage() {
         return errorHeader + stationAlreadyExistsInLine;
+    }
+
+    public static String cannotFoundMessage() {
+        return errorHeader + cannotFound;
+    }
+
+    public static String contentLengthOutOfRangeMessage() {
+        return errorHeader + contentLengthOutOfRange;
     }
 
     public static String requiresIntegerOnlyMessage() {
