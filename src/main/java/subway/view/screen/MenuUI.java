3ba30d87f92a2c9d42cf0442f150c0c619245b22
dreamfinder2019 <dreamfinder2019@gmail.com>
2021-06-01 19:01:43 +0900

    style(screen): MenuUI - Delete Access Modifier

--- src/main/java/subway/view/screen/MenuUI.java
+++ src/main/java/subway/view/screen/MenuUI.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 import subway.domain.Subway;
 
 public interface MenuUI {
-    public void show();
-    public MenuUI commands(Scanner sc, Subway subway);
+
+    void show();
+
+    MenuUI commands(Scanner sc, Subway subway);
 }
