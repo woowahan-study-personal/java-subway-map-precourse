f2360360521eeb0374508ae1910183783794df62
dreamfinder2019 <dreamfinder2019@gmail.com>
2021-06-02 14:31:35 +0900

    refactor: InputMessage - Apply static variable convention

--- src/main/java/subway/view/message/InputMessage.java
+++ src/main/java/subway/view/message/InputMessage.java
@@ -2,54 +2,54 @@ package subway.view.message;
 
 public class InputMessage {
 
-    private static final String inputHeader = "## ";
-    private static final String askFunctionInput = "원하는 기능을 선택하세요.";
-    private static final String askStationNameInput = "역 이름을 입력하세요";
-    private static final String askLineNameInput = "노선 이름을 입력하세요.";
-    private static final String askFromStationNameForAdd = "등록할 노선의 상행 종점역 이름을 입력하세요.";
-    private static final String askToStationNameForAdd = "등록할 노선의 하행 종점역 이름을 입력하세요.";
-    private static final String askStationOrder = "순서를 입력하세요.";
+    private static final String INPUT_HEADER = "## ";
+    private static final String ASK_FUNCTION_INPUT = "원하는 기능을 선택하세요.";
+    private static final String ASK_STATION_NAME_INPUT = "역 이름을 입력하세요";
+    private static final String ASK_LINE_NAME_INPUT = "노선 이름을 입력하세요.";
+    private static final String ASK_FROM_STATION_NAME_FOR_ADD = "등록할 노선의 상행 종점역 이름을 입력하세요.";
+    private static final String ASK_TO_STATION_NAME_FOR_ADD = "등록할 노선의 하행 종점역 이름을 입력하세요.";
+    private static final String ASK_STATION_ORDER = "순서를 입력하세요.";
 
     private InputMessage() {
     }
 
     public static String askStationNameMessageForAdd() {
-        return inputHeader + "등록할 " + askStationNameInput;
+        return INPUT_HEADER + "등록할 " + ASK_STATION_NAME_INPUT;
     }
 
     public static String askStationNameMessageForDelete() {
-        return inputHeader + "삭제할 " + askStationNameInput;
+        return INPUT_HEADER + "삭제할 " + ASK_STATION_NAME_INPUT;
     }
 
     public static String askLineNameMessageForAdd() {
-        return inputHeader + "등록할 " + askLineNameInput;
+        return INPUT_HEADER + "등록할 " + ASK_LINE_NAME_INPUT;
     }
 
     public static String askLineNameMessageForDelete() {
-        return inputHeader + "삭제할 " + askLineNameInput;
+        return INPUT_HEADER + "삭제할 " + ASK_LINE_NAME_INPUT;
     }
 
     public static String askLineNameMessage() {
-        return inputHeader + askLineNameInput;
+        return INPUT_HEADER + ASK_LINE_NAME_INPUT;
     }
 
     public static String askStationNameMessage() {
-        return inputHeader + askStationNameInput;
+        return INPUT_HEADER + ASK_STATION_NAME_INPUT;
     }
 
     public static String askFunctionInputMessage() {
-        return inputHeader + askFunctionInput;
+        return INPUT_HEADER + ASK_FUNCTION_INPUT;
     }
 
     public static String askFromStationNameForAddMessage() {
-        return inputHeader + askFromStationNameForAdd;
+        return INPUT_HEADER + ASK_FROM_STATION_NAME_FOR_ADD;
     }
 
     public static String askToStationNameForAddMessage() {
-        return inputHeader + askToStationNameForAdd;
+        return INPUT_HEADER + ASK_TO_STATION_NAME_FOR_ADD;
     }
 
     public static String askStationOrderMessage() {
-        return inputHeader + askStationOrder;
+        return INPUT_HEADER + ASK_STATION_ORDER;
     }
 }
