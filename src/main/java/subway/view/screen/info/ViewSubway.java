9aef39511396c04d08d38dd4fd528b4939c173e2
dreamfinder2019 <dreamfinder2019@gmail.com>
2021-06-02 14:39:02 +0900

    refactor: ViewSubway - Add Message

--- src/main/java/subway/view/screen/info/ViewSubway.java
+++ src/main/java/subway/view/screen/info/ViewSubway.java
@@ -4,6 +4,7 @@ import java.util.List;
 import subway.domain.Line;
 import subway.domain.Subway;
 import subway.view.message.InfoMessage;
+import subway.view.message.MenuMessage;
 import subway.view.tools.OutputTool;
 
 public class ViewSubway implements ViewUI {
@@ -11,6 +12,9 @@ public class ViewSubway implements ViewUI {
     @Override
     public void commands(Subway subway) {
         List<Line> lineList = subway.getLineList();
+
+        System.out.println(MenuMessage.subwayListPageMessage());
+        System.out.println();
 
         for (Line line : lineList) {
             System.out.println(InfoMessage.getInfoHeader() + line.getName());
