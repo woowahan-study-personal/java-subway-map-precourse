91b98189b1c040ddd41f9db32e14010178556f24
dreamfinder2019 <dreamfinder2019@gmail.com>
2021-06-02 14:23:06 +0900

    refactor: AppStatusCode - refactor static variable convention

--- src/main/java/subway/AppStatusCode.java
+++ src/main/java/subway/AppStatusCode.java
@@ -2,32 +2,32 @@ package subway;
 
 public class AppStatusCode {
 
-    private static final int outOfRange = -1;
-    private static final int notFound = 0;
-    private static final int requestApproved = 1;
-    private static final int contentRangeFailed = 2;
-    private static final int contentAlreadyExists = 3;
+    private static final int OUT_OF_RANGE = -1;
+    private static final int NOT_FOUND = 0;
+    private static final int REQUEST_APPROVED = 1;
+    private static final int CONTENT_RANGE_FAILED = 2;
+    private static final int CONTENT_ALREADY_EXISTS = 3;
 
     private AppStatusCode() {
     }
 
     public static int outOfRangeCode() {
-        return outOfRange;
+        return OUT_OF_RANGE;
     }
 
     public static int notFoundCode() {
-        return notFound;
+        return NOT_FOUND;
     }
 
     public static int requestApprovedCode() {
-        return requestApproved;
+        return REQUEST_APPROVED;
     }
 
     public static int contentRangeFailedCode() {
-        return contentRangeFailed;
+        return CONTENT_RANGE_FAILED;
     }
 
     public static int contentAlreadyExistsCode() {
-        return contentAlreadyExists;
+        return CONTENT_ALREADY_EXISTS;
     }
 }
