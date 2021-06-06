8f59607cb641bbe4336f31afec3e91d030b1ba56
송다혜 <dada3254@daum.net>
2021-06-06 19:57:15 +0900

    refactor(main) : 메인화면 안에 항목별로 메서드로 분리 & 반복/종료조건 추가 1 역관리화면 메서드 추가 2 노선관리화면 메서드 추가 3 구간관리화면 메서드 추가 스캔 입력값을 nextInt 에서 nextLine으로 변경

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -11,6 +11,8 @@ public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO: 프로그램 구현
+        String newLine = System.lineSeparator();
+        // 초기 데이터 입력
         StationRepository stationRepository = new StationRepository();
         LineRepository lineRepository = new LineRepository();
 
@@ -27,134 +29,175 @@ public class Application {
         boolean flag = true;
         while (flag) {
             // A 메인화면
-            System.out.println("## 메인 화면\n"
-                + "1. 역 관리\n"
-                + "2. 노선 관리\n"
-                + "3. 구간 관리\n"
-                + "4. 지하철 노선도 출력\n"
-                + "Q. 종료");
+            System.out.println(newLine + "## 메인 화면" + newLine
+                + "1. 역 관리" + newLine
+                + "2. 노선 관리" + newLine
+                + "3. 구간 관리" + newLine
+                + "4. 지하철 노선도 출력" + newLine
+                + "Q. 종료" + newLine);
             System.out.println("## 원하는 기능을 선택하세요.");
-            int main_func = scanner.nextInt();
+            String main_func = scanner.nextLine();
             // B -1. 역관리
-            if (main_func == 1) {
-                System.out.println("## 역 관리 화면\n"
-                    + "1. 역 등록\n"
-                    + "2. 역 삭제\n"
-                    + "3. 역 조회\n"
-                    + "B. 돌아가기");
-                System.out.println("## 원하는 기능을 선택하세요.");
-                int func = scanner.nextInt();
-                if (func == 1) {
-                    System.out.println("## 등록할 역 이름을 입력하세요.");
-
-                    stationRepository.addStation(new Station(scanner.next()));
-                    System.out.println("[INFO] 지하철 역이 등록되었습니다.");
-                    // 테스트 출력
-                    System.out.println(
-                        stationRepository.stations().get(stationRepository.stations().size() - 1)
-                            .getName());
-                    // A 메인화면 돌아가기/재귀
-                }
-                if (func == 2) {
-                    System.out.println("## 삭제할 역 이름을 입력하세요.");
-
-                    stationRepository.deleteStation(scanner.next());
-                    System.out.println("[INFO] 지하철 역이 삭제되었습니다.");
-                    // 테스트 출력
-                    for (int i = 0; i < stationRepository.stations().size(); i++) {
-                        System.out
-                            .println("[INFO] " + stationRepository.stations().get(i).getName());
-                    }
-                }
-                if (func == 3) {
-                    System.out.println("## 역 목록");
-                    for (int i = 0; i < stationRepository.stations().size(); i++) {
-                        System.out
-                            .println("[INFO] " + stationRepository.stations().get(i).getName());
-                    }
-                }
-                // A 메인화면 돌아가기/재귀
+            if (main_func.equals("1")) {
+                stationManagement(stationRepository, scanner, newLine);
             }
             // B -2. 노선 관리
-            if (main_func == 2) {
-                System.out.println("## 노선 관리 화면\n"
-                    + "1. 노선 등록\n"
-                    + "2. 노선 삭제\n"
-                    + "3. 노선 조회\n"
-                    + "B. 돌아가기");
-                System.out.println("## 원하는 기능을 선택하세요.");
-                int func = scanner.nextInt();
-                if (func == 1) {
-                    System.out.println("## 등록할 노선 이름을 입력하세요.");
-                    Line line = new Line(scanner.next());
-                    lineRepository.addLine(line);
-                    // To DO !!!
-                    System.out.println("## 등록할 노선의 상행 종점역 이름을 입력하세요.");
-                    line.addLineStation(scanner.next());
-                    System.out.println("## 등록할 노선의 하행 종점역 이름을 입력하세요.");
-                    line.addLineStation(scanner.next());
-                    System.out.println("[INFO] 지하철 노선이 등록되었습니다.");
-                    // 테스트 출력 1개
-                    System.out.println(
-                        lineRepository.lines().get(lineRepository.lines().size() - 1)
-                            .getName());
-                    for (int i = 0; i < line.getLineStations().size(); i++) {
-                        System.out
-                            .println("[노선의 역] " + line.getLineStations().get(i));
-                    }
-                    // A 메인화면 돌아가기/재귀
-                }
-                if (func == 2) {
-                    System.out.println("## 삭제할 노선 이름을 입력하세요.");
-
-                    lineRepository.deleteLineByName(scanner.next());
-                    System.out.println("[INFO] 지하철 노선이 삭제되었습니다.");
-                    // 테스트 출력
-                    for (int i = 0; i < lineRepository.lines().size(); i++) {
-                        System.out
-                            .println("[INFO] " + lineRepository.lines().get(i).getName());
-                    }
-                }
-                if (func == 3) {
-                    System.out.println("## 노선 목록");
-                    for (int i = 0; i < lineRepository.lines().size(); i++) {
-                        System.out
-                            .println("[INFO] " + lineRepository.lines().get(i).getName());
-                    }
-                }
+            if (main_func.equals("2")) {
+                lineManagement(lineRepository, scanner, newLine);
             }
             // B -3. 구간 관리
-            if (main_func == 3) {
-                System.out.println("## 구간 관리 화면\n"
-                    + "1. 구간 등록\n"
-                    + "2. 구간 삭제\n"
-                    + "B. 돌아가기");
-                System.out.println("## 원하는 기능을 선택하세요.");
-                int func = scanner.nextInt();
-                if (func == 1) {
-                    System.out.println("## 노선을 입력하세요.");
-                    System.out.println("## 역이름을 입력하세요.");
-                    // String st = scanner.next();
-                    System.out.println("## 순서를 입력하세요.");
-                    // int idx = scanner.nextInt();
-                    System.out.println("[INFO] 구간이 등록되었습니다.");
-                }
-                if (func == 2) {
-                    System.out.println("## 삭제할 구간의 노선을 입력하세요.");
-                    System.out.println("## 삭제할 구간의 역을 입력하세요.");
-                    System.out.println("[INFO] 구간이 삭제되었습니다.");
-                }
-                // A 메인화면 돌아가기
+            if (main_func.equals("3")) {
+                lineSectionManagement(scanner, newLine);
             }
             // B -4. 지하철 노선도 출력
-            if (main_func == 4) {
+            if (main_func.equals("4")) {
                 System.out.println("## 지하철 노선도");
                 // 해당 노선 이름
                 System.out.println("[INFO] ---");
                 // 해당 노선의 역 이름 ...
             }
-
-            flag = false;
+            if (main_func.equalsIgnoreCase("Q")) {
+                flag = false;
+            }
         }
     }
+
+    /**
+     * 1번째 역 관리 화면
+     *
+     * @param stationRepository
+     */
+    private static void stationManagement(StationRepository stationRepository, Scanner scanner,
+        String newLine) {
+        System.out.println(newLine + "## 역 관리 화면" + newLine
+            + "1. 역 등록" + newLine
+            + "2. 역 삭제" + newLine
+            + "3. 역 조회" + newLine
+            + "B. 돌아가기" + newLine);
+        System.out.println("## 원하는 기능을 선택하세요.");
+        String func = scanner.nextLine();
+        if (func.equals("1")) {
+            System.out.println("## 등록할 역 이름을 입력하세요.");
+            stationRepository.addStation(new Station(scanner.next()));
+            System.out.println("[INFO] 지하철 역이 등록되었습니다.");
+            // 테스트 출력
+            System.out.println(
+                stationRepository.stations().get(stationRepository.stations().size() - 1)
+                    .getName());
+        }
+        if (func.equals("2")) {
+            System.out.println("## 삭제할 역 이름을 입력하세요.");
+            stationRepository.deleteStation(scanner.next());
+            System.out.println("[INFO] 지하철 역이 삭제되었습니다.");
+            // 테스트 출력
+            for (int i = 0; i < stationRepository.stations().size(); i++) {
+                System.out
+                    .println("[INFO] " + stationRepository.stations().get(i).getName());
+            }
+        }
+        if (func.equals("3")) {
+            System.out.println("## 역 목록");
+            for (int i = 0; i < stationRepository.stations().size(); i++) {
+                System.out
+                    .println("[INFO] " + stationRepository.stations().get(i).getName());
+            }
+        }
+        if (func.equalsIgnoreCase("B")) {
+            return;
+        }
+    }
+
+    /**
+     * 2번째 노선 관리 화면
+     *
+     * @param lineRepository
+     */
+    private static void lineManagement(LineRepository lineRepository, Scanner scanner,
+        String newLine) {
+        System.out.println(newLine + "## 노선 관리 화면" + newLine
+            + "1. 노선 등록" + newLine
+            + "2. 노선 삭제" + newLine
+            + "3. 노선 조회" + newLine
+            + "B. 돌아가기" + newLine);
+        System.out.println("## 원하는 기능을 선택하세요.");
+        String func = scanner.nextLine();
+        if (func.equals("1")) {
+            System.out.println();
+            System.out.println("## 등록할 노선 이름을 입력하세요.");
+            Line line = new Line(scanner.next());
+            lineRepository.addLine(line);
+            // To DO !!!
+            System.out.println();
+            System.out.println("## 등록할 노선의 상행 종점역 이름을 입력하세요.");
+            line.addLineStation(scanner.next());
+
+            System.out.println();
+            System.out.println("## 등록할 노선의 하행 종점역 이름을 입력하세요.");
+            line.addLineStation(scanner.next());
+
+            System.out.println();
+            System.out.println("[INFO] 지하철 노선이 등록되었습니다.");
+            // 테스트 출력 1개
+            System.out.println(
+                lineRepository.lines().get(lineRepository.lines().size() - 1)
+                    .getName());
+            for (int i = 0; i < line.getLineStations().size(); i++) {
+                System.out
+                    .println("[노선의 역] " + line.getLineStations().get(i));
+            }
+        }
+        if (func.equals("2")) {
+            System.out.println();
+            System.out.println("## 삭제할 노선 이름을 입력하세요.");
+            lineRepository.deleteLineByName(scanner.next());
+            System.out.println();
+            System.out.println("[INFO] 지하철 노선이 삭제되었습니다.");
+            // 테스트 출력
+            for (int i = 0; i < lineRepository.lines().size(); i++) {
+                System.out
+                    .println("[INFO] " + lineRepository.lines().get(i).getName());
+            }
+        }
+        if (func.equals("3")) {
+            System.out.println();
+            System.out.println("## 노선 목록");
+            for (int i = 0; i < lineRepository.lines().size(); i++) {
+                System.out
+                    .println("[INFO] " + lineRepository.lines().get(i).getName());
+            }
+        }
+        if (func.equalsIgnoreCase("B")) {
+            return;
+        }
+    }
+
+    /**
+     * 3번째 구간 관리 화면
+     */
+    private static void lineSectionManagement(Scanner scanner, String newLine) {
+        System.out.println(newLine + "## 구간 관리 화면" + newLine
+            + "1. 구간 등록" + newLine
+            + "2. 구간 삭제" + newLine
+            + "B. 돌아가기" + newLine);
+        System.out.println("## 원하는 기능을 선택하세요.");
+        String func = scanner.nextLine();
+        if (func.equals("1")) {
+            System.out.println("## 노선을 입력하세요.");
+            System.out.println("## 역이름을 입력하세요.");
+            // String st = scanner.next();
+            System.out.println("## 순서를 입력하세요.");
+            // int idx = scanner.nextInt();
+            System.out.println("[INFO] 구간이 등록되었습니다.");
+        }
+        if (func.equals("2")) {
+            System.out.println("## 삭제할 구간의 노선을 입력하세요.");
+            System.out.println("## 삭제할 구간의 역을 입력하세요.");
+            System.out.println("[INFO] 구간이 삭제되었습니다.");
+        }
+        if (func.equalsIgnoreCase("B")) {
+            return;
+        }
+    }
+
+
 }
