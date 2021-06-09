67a42e2e75ab278be08d5a38d8240bdbe178e332
Song Eun Seok <eunsuk93@naver.com>
2021-06-09 13:25:08 +0900

    feat: 노선 init

--- src/main/java/subway/domain/InitSystem.java
+++ src/main/java/subway/domain/InitSystem.java
@@ -5,7 +5,7 @@ public class InitSystem {
     private final StationRepository stationRepository = new StationRepository();
     private final LineRepository lineRepository = new LineRepository();
 
-    public void initStations() {
+    private void initStations() {
         stationRepository.addStation(new Station("교대역"));
         stationRepository.addStation(new Station("강남역"));
         stationRepository.addStation(new Station("역삼역"));
@@ -13,6 +13,12 @@ public class InitSystem {
         stationRepository.addStation(new Station("양재역"));
         stationRepository.addStation(new Station("양재시민의숲역"));
         stationRepository.addStation(new Station("매봉역"));
+    }
+
+    private void initLines() {
+        LineRepository.addLine(new Line("2호선"));
+        LineRepository.addLine(new Line("3호선"));
+        LineRepository.addLine(new Line("신분당선"));
     }
 
 }
