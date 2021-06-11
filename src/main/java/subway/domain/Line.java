0c8677cb9ce4234d0b74749f5b30431b955e24aa
멍토 <daum7766@naver.com>
2021-06-11 16:25:53 +0900

    feat: 구간기능 추가

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -3,8 +3,13 @@ package subway.domain;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import subway.exception.LineException;
+import subway.exception.StationException;
+import subway.exception.SubwayException;
 
 public class Line {
+
+    private static final int MIN_STATION_SIZE = 2;
 
     private final Name name;
     private final List<Station> stations;
@@ -18,8 +23,42 @@ public class Line {
         this.stations = new ArrayList<>(stations);
     }
 
-    public void removeStation(Station station) {
+    public void addStation(int index, Station station) {
+        validateIndex(index);
+        validateAddStation(station);
+        index = Math.min(stations.size(), index);
+        stations.add(index, station);
+    }
 
+    private void validateIndex(int index) {
+        if (index < 0) {
+            throw new SubwayException(LineException.ILLEGAL_INDEX);
+        }
+    }
+
+    private void validateAddStation(Station station) {
+        if (stations.contains(station)) {
+            throw new SubwayException(StationException.DUPLICATE_STATION);
+        }
+    }
+
+    public void removeStation(Station station) {
+        validateRemoveStation(station);
+        validateDeleteSize();
+
+        stations.remove(station);
+    }
+
+    private void validateRemoveStation(Station station) {
+        if (!stations.contains(station)) {
+            throw new SubwayException(LineException.NOT_FOUND_LINE_IN_STATION);
+        }
+    }
+
+    private void validateDeleteSize() {
+        if (stations.size() <= MIN_STATION_SIZE) {
+            throw new SubwayException(LineException.INVALID_STATION_DELETE);
+        }
     }
 
     public String getName() {
--- src/main/java/subway/exception/LineException.java
+++ src/main/java/subway/exception/LineException.java
@@ -2,13 +2,15 @@ package subway.exception;
 
 public enum LineException implements SubwayMapException {
     DUPLICATE_LINE("중복된 노선입니다."),
-    NOT_FOUND_LINE("존재하지 않는 노선입니다.");
+    NOT_FOUND_LINE("존재하지 않는 노선입니다."),
+    ILLEGAL_INDEX("유효하지 않은 인덱스입니다,"),
+    NOT_FOUND_LINE_IN_STATION("노선 안에 존재하지 않는 역입니다."),
+    INVALID_STATION_DELETE("노선은 최소 2개의 역을 가지고 있어야 합니다.");
 
     private final String message;
 
     LineException(String message) {
-        this.message = message;
-    }
+        this.message = message; }
 
     @Override
     public String getMessage() {
